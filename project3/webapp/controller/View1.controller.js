aa9e21aa5de4fa15eac4353bd921ed28a7a05d9b
Ayon Dwary <ayon.dwary@capgemini.com>
2025-10-27 06:13:10 +0000

    first commit

---
+++ project3/webapp/controller/View1.controller.js
@@ -0,0 +1,47 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    'sap/m/MessageToast',
+	"sap/m/MenuItem",
+    'sap/ui/model/json/JSONModel'
+], (Controller,MessageToast,MenuItem) => {
+    "use strict";
+
+    return Controller.extend("com.cg.ayon.project3.controller.View1", {
+        onInit() {
+        },
+
+        onButtonPress: function(oEvent) {
+			var oButton = oEvent.getSource();
+			this.byId("actionSheet").openBy(oButton);
+		},
+
+
+        onMenuAction: function(oEvent) {
+				var oItem = oEvent.getParameter("item"),
+					sItemPath = "";
+
+				while (oItem instanceof MenuItem) {
+					sItemPath = oItem.getText() + " > " + sItemPath;
+					oItem = oItem.getParent();
+				}
+
+				sItemPath = sItemPath.substr(0, sItemPath.lastIndexOf(" > "));
+
+				MessageToast.show("Action triggered on item: " + sItemPath);
+			},
+
+        handleRefresh : function() {
+            location.reload();
+        },
+        onNextPage : function(){
+            // MessageToast.show("Next Page button pressed ");
+              var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                oRouter.navTo("RouteView2");
+
+        },
+  
+
+
+
+    });
+});
